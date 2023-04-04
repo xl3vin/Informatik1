c2f31fa88270fdd6f4f259340d14feeea92be858
Andreas Baumgart <andreas.baumgart@jade-hs.de>
2023-04-04 19:05:38 +0200

    update
    Vorlesungsinhalte vom 4. April 2023

---
+++ src/uebungen/u0/Program.java
@@ -0,0 +1,38 @@
+package uebungen.u0;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// Deklaration der Variablen
+		int hoehe=20;
+		double zeitErde, zeitMond, zeitJupiter;
+		double gKonstanteErde=9.81;
+		double gKonstanteMond=1.61;
+		double gKonstanteJupiter=24.79;
+		String ausgabe ="Fallzeit auf";
+		
+		zeitErde = Math.sqrt(2*hoehe/gKonstanteErde);	
+		zeitMond = Math.sqrt(2*hoehe/gKonstanteMond);
+		zeitJupiter = Math.sqrt(2*hoehe/gKonstanteJupiter);
+		System.out.print(ausgabe+" der Erde: "+zeitErde);
+		System.out.println(ausgabe+ " dem Mond: "+zeitMond);
+		System.out.println(ausgabe+" dem Jupiter: "+zeitJupiter);
+		
+		// Kommentarzeile
+		// Ergänzung aus der Vorlesung
+		// Java-Folien: Seite 
+		// Implizite Typanpassung:
+		
+		byte summe = 100;
+		// Implizit, Automatisch: Jeder kleinere Datentyp passt in einen größeren Datentyp
+		int gesamtesumme = summe; // Kein Problem
+		
+		int zwischenstand = 1000;
+		// summe = zwischenstand; << Erzeugt einen Fehler. Wert passt nicht in Variable vom Datentyp byte
+		// Explizite Typanpassung
+		
+		summe = (byte)1000; // explizite Typanpassung
+		
+		
+	}
+}
